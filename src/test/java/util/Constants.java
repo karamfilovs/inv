146b7971255c30636ef2537fe43cf078d803f192
Aleksandar Karamfilov <karamfilovs@gmail.com>
2023-05-10 22:13:03 +0300

    Update Constants.java

--- src/test/java/util/Constants.java
+++ src/test/java/util/Constants.java
@@ -2,6 +2,6 @@ package util;
 
 public class Constants {
     public static final String USERNAME = "karamfilovs@gmail.com";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "111111";
     public static final String INV_URL = "https://st2016.inv.bg";
 }
